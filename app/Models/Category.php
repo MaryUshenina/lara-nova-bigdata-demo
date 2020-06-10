98aadaeab5de0eb6a7343cc8433509121497294b
Mary Ushenina <ushenina_ma@groupbwt.com>
2020-06-10 15:04:06 +0300

    add category nova resource

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     //
+    public function parentCategory()
+    {
+        return $this->belongsTo(Category::class, 'pid');
+    }
+
+    public function childrenCategories()
+    {
+        return $this->hasMany(Category::class, 'pid');
+    }
+
 }
