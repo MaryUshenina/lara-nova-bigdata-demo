073c3b78417927b56406fc14bf6a70332275b742
Mary Ushenina <ushenina_ma@groupbwt.com>
2020-06-23 14:13:39 +0300

    change category nova resourve for using EagerCategory

--- app/Nova/Category.php
+++ app/Nova/Category.php
@@ -3,9 +3,11 @@
 namespace App\Nova;
 
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 use Laravel\Nova\Fields\BelongsTo;
 use Laravel\Nova\Fields\HasMany;
 use Laravel\Nova\Fields\ID;
+use Laravel\Nova\Fields\Select;
 use Laravel\Nova\Fields\Text;
 use Laravel\Nova\Http\Requests\NovaRequest;
 
@@ -16,7 +18,7 @@ class Category extends Resource
      *
      * @var string
      */
-    public static $model = \App\Models\Category::class;
+    public static $model = \App\Models\EagerCategory::class;
 
     /**
      * The single value that should be used to represent the resource when being displayed.
@@ -30,11 +32,13 @@ class Category extends Resource
      *
      * @var array
      */
-    public static $search = [
-        'id',
-    ];
+    public static $search = [];
 
-    const DEFAULT_INDEX_ORDER = 'id';
+    public static $perPageOptions = [10];
+
+    public static $perPageViaRelationship = 10;
+
+    private static $allCategoriesOptions = [];
 
     /**
      * Get the fields displayed by the resource.
@@ -45,19 +49,9 @@ class Category extends Resource
     public function fields(Request $request)
     {
         return [
-            ID::make()->sortable(),
+            $this->getParentField(),
 
-            BelongsTo::make(__('Parent category'), 'parentCategory', Category::class)
-                ->nullable()
-                ->sortable(),
-
-            Text::make(__('Name'), 'name')
-                ->rules('required', 'max:255'),
-
-            Text::make(__('Subcategories'), function () {
-                return $this->childrenCategories()->count();
-            })
-                ->asHtml(),
+            $this->getNameField(),
 
             HasMany::make(__('Children categories'), 'childrenCategories', Category::class),
         ];
@@ -110,11 +104,70 @@ class Category extends Resource
 
     public static function indexQuery(NovaRequest $request, $query)
     {
-        $query->when(empty($request->get('orderBy')), function ($q) {
-            $q->getQuery()->orders = [];
-
-            return $q->orderBy(static::DEFAULT_INDEX_ORDER);
-        });
+        $query
+            ->when($request->isResourceIndexRequest() && !$request->viaRelationship(), function ($q) use ($request) {
+                return $q->rootLevel();
+            });
     }
 
+    protected static function applyOrderings($query, array $orderings)
+    {
+        return $query->orderByTree();
+    }
+    
+    protected static function fillFields(NovaRequest $request, $model, $fields)
+    {
+        //switch to original model
+        self::$model = \App\Models\Category::class;
+        $model = $model->originalCategory ?? new self::$model();
+
+        $fillFields = parent::fillFields($request, $model, $fields);
+
+        //switch back to EagerCategory
+        self::$model = \App\Models\EagerCategory::class;
+        return $fillFields;
+    }
+
+
+    /**
+     * @return Select
+     */
+    private function getParentField()
+    {
+        if (!count(self::$allCategoriesOptions)) {
+            $all = \App\Models\EagerCategory::select('id')
+                ->addSelect(\DB::raw("CONCAT(REPEAT('--', max_level), ' ',`name`) AS `name` "))
+                ->orderByTree()
+                ->get()
+                ->pluck('name', 'id')->toArray();
+
+            self::$allCategoriesOptions = [0 => 'root'] + $all; // dont use array_merge to keep keys
+        }
+
+        return Select::make(__('Parent'), 'pid')
+            ->options(self::$allCategoriesOptions)
+            ->rules('required', Rule::notIn([$this->id ?? -1]))
+            ->onlyOnForms()
+            ->sortable();
+    }
+
+    /**
+     * @return Text
+     */
+    private function getNameField()
+    {
+        return Text::make(__('Name'), 'name')
+            ->displayUsing(function () {
+
+                $indent = '';
+                if ($this->max_level) {
+                    $indent = str_repeat('--', $this->max_level) . '&nbsp;';
+                }
+                return $indent . $this->name;
+            })
+            ->rules('required', 'max:255')
+            ->asHtml()
+            ->sortable();
+
+    }
 }
