75ba8f3c03010289afaaf93f65c0b0bc312773f2
Mary Ushenina <ushenina_ma@groupbwt.com>
2020-06-30 17:25:51 +0300

    change debugbar including logic

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        if ($this->app->environment() !== 'production') {
+        if (config('app.debug', false)) {
             $this->app->register(\Barryvdh\Debugbar\ServiceProvider::class);
         }
     }
