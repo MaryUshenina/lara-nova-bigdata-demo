6ac89ca65a99aa592b0ce8602cb416316388595e
Mary Ushenina <ushenina_ma@groupbwt.com>
2020-07-06 18:09:58 +0300

    add metrics from dashboard to caching

--- app/Jobs/GenerateMetricsCache.php
+++ app/Jobs/GenerateMetricsCache.php
@@ -5,12 +5,16 @@ namespace App\Jobs;
 use App\Models\Ad;
 use App\Nova\Metrics\AdsAvailability;
 use App\Nova\Metrics\AdsPrices;
+use App\Nova\Metrics\NewAds;
+use App\Nova\Metrics\NewUsers;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+
+use \Laravel\Nova\Http\Requests\NovaRequest;
 
 class GenerateMetricsCache implements ShouldQueue
 {
@@ -35,5 +39,20 @@ class GenerateMetricsCache implements ShouldQueue
     {
         AdsPrices::getCalculatedData('all', Ad::query());
         AdsAvailability::getCalculatedData('all', Ad::query());
+
+        $newAdsMetric = new NewAds();
+        foreach($newAdsMetric->ranges() as $range => $rangeLabel){
+            $fakeRequest = new NovaRequest();
+            $fakeRequest->range = $range;
+            $newAdsMetric->getCalculatedDataByRange($range, $fakeRequest);
+        }
+
+        $newUsersMetric = new NewUsers();
+        foreach($newUsersMetric->ranges() as $range => $rangeLabel){
+            $fakeRequest = new NovaRequest();
+            $fakeRequest->range = $range;
+            $newUsersMetric->getCalculatedDataByRange($range, $fakeRequest);
+        }
+
     }
 }
