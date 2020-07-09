5d01434aced936076417b842b7df07050a720236
Mary Ushenina <ushenina_ma@groupbwt.com>
2020-07-09 11:48:45 +0300

    change AdResourceCountController move  isAnyFilterApplied to trait

---
+++ app/Nova/Requests/IsFilteredTrait.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Nova\Requests;
+
+use Laravel\Nova\Http\Requests\NovaRequest;
+
+trait IsFilteredTrait
+{
+    /**
+     * check if any filter was applied
+     *
+     * @param NovaRequest $request
+     * @return bool
+     */
+    public static function isAnyFilterApplied(NovaRequest $request)
+    {
+        if ($request->has('filters')) {
+            // Get the decoded list of filters
+            $filters = json_decode(base64_decode($filterKey = $request->filters)) ?? [];
+
+            foreach ($filters as $filter) {
+                if (empty($filter->value)) {
+                    continue;
+                }
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
